a12622e16faef0aecc620f8ee9310d5c731f8314
Nigar Jafar <nigar.c@code.edu.az>
2016-09-10 23:47:20 +0400

    Navbar Responsive 50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,24 @@ jQuery(document).ready(function($) {
     
     
 //Navbar
+    
+    
 $("#header .dropdown").mouseenter(function(){
+    if( $(window).width()>768){
+    
     $(this).find(">a").css("color","#F44336")
     $(this).find(".dropdown-menu").slideDown()
-    
-})
+    }
+ })
 $("#header .dropdown").mouseleave(function(){
+    if( $(window).width()>768){
     $(this).find(">a").css("color","white")
     $(this).find(".dropdown-menu").slideUp();
+    }
+ })
+
+$(window).resize(function(){
+    console.log( $(window).width())
 })
 
 });
