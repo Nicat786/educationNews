b692de33ff676b49c2c5a714b8877781c80b3ab2
Nigar Jafar <nigar.c@code.edu.az>
2016-09-10 13:44:36 +0400

    Navbar Footer (Not Responsive)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
 jQuery(document).ready(function($) {
 	console.log('ready!!!')
+    
+    
+//Navbar
+$("#header .dropdown").mouseenter(function(){
+    $(this).find(">a").css("color","#F44336")
+    $(this).find(".dropdown-menu").slideDown()
+    
+})
+$("#header .dropdown").mouseleave(function(){
+    $(this).find(">a").css("color","white")
+    $(this).find(".dropdown-menu").slideUp();
+})
+
 });
